27f5bac39cc319396ba9c6707da023c0c806c245
LivingLandscapes <81996944+LivingLandscapes@users.noreply.github.com>
2026-08-30 15:00:13 -0500

    Hello bayes lab and exercise code.

--- code/04-A-hello-bayes-exercise-code.R
+++ code/04-A-hello-bayes-exercise-code.R
@@ -47,48 +47,144 @@ whistle <-
               mutate(Route.Number = Route) %>%
               group_by(Route.Number) %>%
               filter(row_number() == 1)) %>%
-  mutate(YearNum = Year - 1964,
-         YearNum2 = (Year - 1964)^2,
-         Easting_scaled = scale(UTMYE),
-         Northing_scaled = scale(UTMXN))
+  mutate(across(c(Year, UTMYE, UTMXN),
+                function(X) (X - mean(X)) / sd(X),
+                .names = "{.col}_scaled"))
 
 #=============================================================================
-## Frequentist
+## Frequentist vs. Bayesian
 
+# Fit a linear regression with the frequentist method
 fit_lm <-
-  lm(WhistleCount ~ YearNum + YearNum2 + Easting_scaled * Northing_scaled, 
+  lm(WhistleCount ~ Year_scaled + UTMYE_scaled * UTMXN_scaled, 
      data = whistle)
-summary(fit_lm)
-plot(fit_lm)
-fit_glm <-
-  glm((WhistleCount + 0.0001)  ~ YearNum + YearNum2 + Easting_scaled * Northing_scaled, 
-      family = Gamma("log"),
-      data = whistle)
-summary(fit_glm)
-plot(fit_glm)
+
+# Fit the same model with Bayesian methods using the `brms` package!
+fit_brm <-
+  # Workhorse function: `brm`
+  brm(
+      # Formula argument--just like `lm` or `glm`!
+      formula = WhistleCount  ~ Year_scaled + UTMYE_scaled * UTMXN_scaled,
+      # Family argument is just like `glm` but with more options.
+      family = gaussian,
+      # Data is same too
+      data = whistle,
+      # This is new: it's the number of MCMC chains to run. 3 - 4 are
+      # sufficient, but if you're just playing around and want speed, you can
+      # run one.
+      chains = 3,
+      # Number of "warmup" iterations to get the MCMC chain started. Warmup
+      # iterations are not used for posterior distributions. The default is
+      # iter/2.
+      warmup = 500, 
+      # Number of iterations to run on each MCMC chain. This includes warmup.
+      iter = 1000,
+      # How many iterations to save. Can be useful to increase if iter is very
+      # large to reduce memory and computation time.
+      thin = 1,
+      # Number of cores to use. Can speed up processing if you're using >1 chain.
+      cores = 3)
+
+# Check out the summary
+fit_brm
+
+### Challenge #1: 
+
+# Compare the frequentist and Bayesian model outputs/summary tables,
+# particularly the coefficient estimates and standard errors. Are there
+# differences? If so, what are they?
 
 #=============================================================================
-## Bayesian
+## Model diagnostics
 
-# Let students run a model.
+# Posterior predictive check. Ideally, the `yrep` lines should overlap the black
+# `y` line. If there are systemmatic problems, the model is probably not a great
+# fit. Do you see any issues?
+pp_check(fit_brm)
 
-# Work through brm function.
-fit_brm <- 
-  brm(bf(WhistleCount  ~ YearNum, 
-         hu = ~ YearNum),
-      family = hurdle_lognormal(),
+# Look at traceplots for each parameter estimate
+plot(fit_brm)
+
+# Bayesian R-squared
+bayes_R2(fit_brm)
+
+#=============================================================================
+## Predicting
+
+# Predict whistle counts. Note how similar to frequentist syntax!
+pred_df <- 
+  cbind(whistle,
+        predict(fit_brm, 
+                whistle,
+                probs = c(0.025, 0.975)))
+
+# Plot predictions
+ggplot(pred_df,
+       aes(x = Year, 
+           y = Estimate, 
+           ymin = Q2.5, 
+           ymax = Q97.5,
+           color = Route.Name, 
+           group = Route.Name,
+           fill = Route.Name)) + 
+  scale_color_viridis_d() + 
+  scale_fill_viridis_d() + 
+  geom_ribbon(alpha = 0.25, color = NA) + 
+  geom_line(linewidth = 1.5) +
+  theme_bw() + 
+  theme(axis.text = element_text(size = 11),
+        axis.title = element_text(size = 14))
+
+### What's the problem with these predictions?!
+
+#=============================================================================
+## brms has a ton of bells and ...whistles... 
+
+# We can fix a key issue by truncating the normal distribution at zero!
+fitTrunc0_brm <-
+  brm(WhistleCount | trunc(lb = 0)  ~ Year_scaled + UTMYE_scaled * UTMXN_scaled,
+      family = gaussian(),
       data = whistle,
       chains = 3,
-      iter = 2000,
+      iter = 1000,
       threads = 3)
-fit_brm
-plot(fit_brm)
-nd <- 
-  data.frame(YearNum = sort(unique(whistle$Year)),
-             YearNum2 = sort(unique(whistle$YearNum2)))
-pred_df <- 
-  predict(fit_brm, 
-          nd,
-          # type = "response",
-          probs = c(0.025, 0.975))
-head(pred_df)
+
+# Predict whistle counts with new zero truncated model
+predTrunc0_df <- 
+  cbind(whistle,
+        predict(fitTrunc0_brm, 
+                whistle,
+                probs = c(0.025, 0.975)))
+
+# Is the nonsense issue solved?
+ggplot(predTrunc0_df,
+       aes(x = Year, 
+           y = Estimate, 
+           ymin = Q2.5, 
+           ymax = Q97.5,
+           color = Route.Name, 
+           group = Route.Name,
+           fill = Route.Name)) + 
+  scale_color_viridis_d() + 
+  scale_fill_viridis_d() + 
+  geom_ribbon(alpha = 0.25, color = NA) + 
+  geom_line(linewidth = 1.5) +
+  theme_bw() + 
+  theme(axis.text = element_text(size = 11),
+        axis.title = element_text(size = 14))
+
+### Challenge #2:
+
+# Compare the posterior predictive checks between the simple brms model and the
+# zero truncated model. Which is a better fit?
+
+#=============================================================================
+## Generalized linear models in brms
+
+### Challenge #3:
+
+# 1. There's a potential problem with the zero truncated model. Explore the data
+# to figure out what it is.
+
+# 2. "Extra hard" challenge: how can we solve this potential problem with
+# frequentist OR Bayesian methods?
